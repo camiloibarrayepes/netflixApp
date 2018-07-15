2a9c38052dc778af6298f7eda985c8d182ab6132
camilo andres ibarra yepes <cibarra@grupoalto.com>
2018-07-15 17:26:33 -0500

    se agrego react-native-swiper para el slider, se creó el componente Slider.js y se llama desde app.js

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ import{
 } from 'react-native'
 
 import List from './components/List'
+import Slide from './components/Slider'
+
 
 class App extends Component {
 	render(){
 		return(
 			<View style={[{flex: 1}, styles.containter]}>
+				<Slide />
 				<List />
 			</View>
 		)
