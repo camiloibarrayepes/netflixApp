406f3bd702e25ebe50eb128307a38dbf74eafaad
camilo andres ibarra yepes <cibarra@grupoalto.com>
2018-07-16 23:02:17 -0500

    se quito animación de transición para vista de busqueda

--- src/indexApp.js
+++ src/indexApp.js
@@ -6,6 +6,20 @@ import {
 import App from './app'
 import Search from './components/Search'
 
+import buildStyleInterpolator from 'buildStyleInterpolator'
+
+const NoTransition = {
+	opacity:{
+		from: 1,
+		to: 1,
+		min: 1,
+		max: 1,
+		type: 'linear',
+		extrapolate: false,
+		round: 100
+	}
+}
+
 class IndexApp extends Component{
 
 	_renderScene(route, navigator){
@@ -23,11 +37,28 @@ class IndexApp extends Component{
 
 		}
 	}
+
+	_configureScene(route, routeStack){
+		switch(route.ident){
+			case 'Search':
+				return{
+					...Navigator.SceneConfigs.FloatFromLeft,
+					gestures: null,
+					defaultTransitionVelocity: 100,
+					animationInterpolators: {
+						into: buildStyleInterpolator(NoTransition),
+						out: buildStyleInterpolator(NoTransition)
+					}
+				}
+		}
+	}
+
 	render(){
 		return(
 			<Navigator
 				initialRoute={{ident: 'App'}}
 				renderScene={this._renderScene}
+				configureScene={this._configureScene}
 
 			/>
 		)
