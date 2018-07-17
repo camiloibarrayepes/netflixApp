4b72e0c782d997e648f3d88a662d403225adb68e
camilo andres ibarra yepes <cibarra@grupoalto.com>
2018-07-17 12:22:41 -0500

    TabsEpisodes funcionando diseño (version 2018)

---
+++ src/components/TabsEpisodes.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import {View,Dimensions,Platform, StyleSheet} from 'react-native';
+import {SceneMap,TabView, TabBar,PagerPan,PagerScroll } from 'react-native-tab-view';
+import Episodes from './Episodes'
+import Trailers from './Trailers'
+
+class TabsEpisodes extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			index: 0,
+			routes: [
+				{key: '1', title: 'Episodes'},
+				{key: '2', title: 'Trailers & More'}
+			]
+		}
+	}
+
+	_handleChangeTab(index){
+		this.setState({index})
+	}
+
+	_renderHeader(props){
+		return <TabBar {...props} />
+	}
+
+	_renderScene({route}){
+		switch(route.key){
+			case '1':
+				return <Episodes />
+			case '2':
+				return <Trailers />
+			default:
+				return null
+		}
+	}
+
+	 _renderPager(props){
+     return (Platform.OS === 'ios') ? <PagerScroll {...props} /> : <PagerPan {...props} />
+   	}
+	render(){
+		return(
+			 <TabView
+	          navigationState={this.state}
+	          renderScene={this._renderScene}
+	          renderTabBar={this._renderHeader}
+	          onIndexChange={this._handleChangeTab.bind(this)}
+	          initialLayout={{
+	            width: Dimensions.get('window').width,
+	            height: Dimensions.get('window').height
+	          }}
+	          renderPager={this._renderPager}
+	        />
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+})
+
+export default TabsEpisodes
---
+++ src/components/Trailers.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import{
+	View,
+	Text
+} from 'react-native'
+
+
+class Trailers extends Component{
+	render(){
+		return(
+			<Text>Trailers</Text>
+		)
+	}
+}
+
+export default Trailers
